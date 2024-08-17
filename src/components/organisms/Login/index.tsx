0411d78f5386a7e695a3335d7a218094e5212802
Chiranjeev Thapliyal <6258860@gmail.com>
2024-08-17 16:58:11 +0530

    feat: added password toggle with eye icon in login

--- src/components/organisms/Login/index.tsx
+++ src/components/organisms/Login/index.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Button, Input, Label, Text } from "../../atoms";
 import { Card } from "../../molecules";
 import { Form } from "../../organisms";
 
 function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+  const toggleShowPassword = () => setShowPassword(prev => !prev);
+
   return (
     <Card className="min-w-max max-w-116 p-[1px] bg-gradient-to-br from-gradient-start to-gradient-stop animate">
       <Card className="py-10 px-6 bg-black-light font-medium ">
@@ -14,7 +18,10 @@ function Login() {
           >
             Welcome Back
           </Text>
-          <Text as="h1" className="text-center text-lg text-white font-semibold">
+          <Text
+            as="h1"
+            className="text-center text-lg text-white font-semibold"
+          >
             Log into your account
           </Text>
         </div>
@@ -22,14 +29,33 @@ function Login() {
         <Form className="pt-11">
           <div className="flex flex-col gap-2">
             <Label>Email or Username</Label>
-            <Input className="text-gray-heavy" placeholder="Enter your email or username" type="text" />
+            <Input
+              className="text-white"
+              placeholder="Enter your email or username"
+              type="text"
+            />
           </div>
           <div className="flex flex-col gap-2">
             <div className="flex justify-between">
               <Label>Password</Label>
               <Label className="text-xs">Forgot password?</Label>
             </div>
-            <Input className="text-gray-heavy" placeholder="Enter your password" type="password" />
+            <div className="relative flex items-center">
+              <Input
+                className="order-1 w-full text-white"
+                placeholder="Enter your password"
+                type={showPassword ? "text" : "password"}
+              />
+              <span onClick={toggleShowPassword} className="absolute right-0 pr-4">
+                <img
+                  className="w-fit h-fit relative before:content-[''] before:absolute before:w-full before:h-[2px] before:bg-gray-heavy before:top-[50%] before:left-0 before:rotate-45"
+                  src="/icons/eye.svg"
+                />
+              </span>
+
+
+              {/* <img src="" /> */}
+            </div>
           </div>
           <Button className="w-full">Login now</Button>
         </Form>
