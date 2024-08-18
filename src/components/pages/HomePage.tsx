c95e3e9da77ade838b414a338baf87e63277dca1
Chiranjeev Thapliyal <6258860@gmail.com>
2024-08-18 20:15:28 +0530

    fix: bifurcated login page with styles modified

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import { Text } from "../atoms";
+import { Modal } from "../molecules";
+import { Login, NewPost, Post, Register } from "../organisms";
 import Data from "../../store/data.json";
-import { NewPost, Post } from "../organisms";
 
 function HomePage() {
+  const [showLoginModal, setShowLoginModal] = useState(false);
+  const [showRegisterModal, setShowRegisterModal] = useState(false);
+
   const { posts } = Data;
+
+  const openLoginModal = () => setShowLoginModal(true);
+
+  const toggleAuth = () => {
+    setShowLoginModal((prev) => !prev);
+    setShowRegisterModal((prev) => !prev);
+  }
+
+  const closeModal = () => {
+    setShowLoginModal(false);
+    setShowRegisterModal(false);
+  }
+
   return (
-    <div className="w-full h-full bg-black flex flex-col items-center p-16">
-      <div className="container w-1/2 max-w-175">
+    <div className="w-full h-full bg-black flex flex-col items-center p-4 md:p-16 overflow-y-auto">
+      <div className="container md:w-1/2 md:max-w-175">
         <div className="flex flex-col gap-3">
           <Text as="h2" className="text-3xl text-gray-normal font-medium">
             Hello Jane
@@ -21,13 +39,20 @@ function HomePage() {
         </div>
         <section className="flex flex-col gap-4">
           <div className="w-full pt-10">
-            <NewPost />
+            <NewPost onClick={openLoginModal} />
           </div>
           {posts?.map((post) => (
-            <Post {...post} />
+            <Post key={post.id} {...post} />
           ))}
         </section>
       </div>
+      <Modal isOpen={showLoginModal || showRegisterModal} closeModal={closeModal}>
+        {showLoginModal ? (
+          <Login onClose={closeModal} onSignupClick={toggleAuth} />
+        ) : showRegisterModal ? (
+          <Register onClose={closeModal} onLoginClick={toggleAuth} />
+        ) : null}
+      </Modal>
     </div>
   );
 }
--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,10 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import { Login } from "../organisms";
 
 function LoginPage() {
+  const navigate = useNavigate();
+  const onSignupClick = () => navigate("/register");
+
   return (
-    <div className="h-full w-full flex flex-col justify-center items-center bg-black gap-12 p-4">
+    <div className="h-full w-full flex flex-col justify-center items-center bg-black gap-12 p-4 overflow-y-auto">
       <img src="/logo.svg" className="w-10 h-10" />
-      <Login />
+      <Login onSignupClick={onSignupClick} />
     </div>
   );
 }
