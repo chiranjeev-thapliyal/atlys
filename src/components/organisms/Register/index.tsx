0bc9f0f29a956faff2d6ee1bfab7cd83fe3c5a54
Chiranjeev Thapliyal <6258860@gmail.com>
2024-08-18 21:46:06 +0530

    fix: hide/show password added in register component

--- src/components/organisms/Register/index.tsx
+++ src/components/organisms/Register/index.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import { Button, Input, Label, Text } from "../../atoms";
 import { Card } from "../../molecules";
 import { Form } from "../../organisms";
 import { RegisterProps } from "./index.types";
 
 function Register({onClose, onLoginClick, onSubmit}: RegisterProps) {
+  const [showPassword, setShowPassword] = useState(false);
+  const toggleShowPassword = () => setShowPassword((prev) => !prev);
+
   return (
     <Card className="relative min-w-max max-w-116 p-[1px] bg-gradient-to-br from-gradient-start to-gradient-stop animate">
     <Card className="min-w-max max-w-116 p-6 bg-black-light font-medium flex flex-col">
@@ -30,7 +34,22 @@ function Register({onClose, onLoginClick, onSubmit}: RegisterProps) {
         </div>
         <div className="flex flex-col gap-2">
           <Label>Password</Label>
-          <Input className="text-white" type="text" placeholder="Choose a strong password" />
+          <div className="relative flex items-center">
+              <Input
+                className="order-1 w-full text-white"
+                placeholder="Choose a strong password"
+                type={showPassword ? "text" : "password"}
+              />
+              <span
+                onClick={toggleShowPassword}
+                className="absolute right-0 pr-4"
+              >
+                <img
+                  className="w-fit h-fit relative before:content-[''] before:absolute before:w-full before:h-[2px] before:bg-gray-heavy before:top-[50%] before:left-0 before:rotate-45"
+                  src="/icons/eye.svg"
+                />
+              </span>
+            </div>
         </div>
         <div className="pt-5">
           <Button onClick={onSubmit} className="w-full bg-blue-normal">Continue</Button>
